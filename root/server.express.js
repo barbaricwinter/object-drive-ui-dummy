1441484a90ae58940a140676c9915728531025b8
Emory Merryman <emory.merryman@deciphernow.com>
2017-03-26 14:22:09 +0000

    fix server

--- root/server.express.js
+++ root/server.express.js
@@ -1,20 +1,3 @@
-{
-  "name": "dummyauth",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "",
-  "license": "ISC",
-  "devDependencies": {
-    "express": "^4.14.0"
-  }
-}
-/srv/object-drive-ui-compose # cat dockers/dummy/
-Dockerfile         chm_drive          me                 package.json       run.sh             server.express.js
-/srv/object-drive-ui-compose # cat dockers/dummy/server.express.js 
 (function(express, join){
     "use strict";
     return express()
